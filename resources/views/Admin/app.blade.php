396dd58409984922472036e29a992c98d7d33712
meimanjayagea <jayameiman17@gmail.com>
2021-08-24 17:10:43 +0700

    update app

--- resources/views/Admin/app.blade.php
+++ resources/views/Admin/app.blade.php
@@ -10,8 +10,7 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
-    <!-- Theme style -->
+
     <!-- IonIcons -->
     <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
