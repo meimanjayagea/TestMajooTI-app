e7e0165d9f1f189578062ebc5e681cc9d3e1cbd0
meimanjayagea <jayameiman17@gmail.com>
2021-08-24 17:13:28 +0700

    update terbaru

--- resources/views/Admin/app.blade.php
+++ resources/views/Admin/app.blade.php
@@ -13,23 +13,23 @@
 
     <!-- IonIcons -->
     <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/dist/css/adminlte.min.css')}}">
 
     <!-- Tempusdominus Bbootstrap 4 -->
     <link rel="stylesheet"
-        href="{{asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+        href="{{asset('storage/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
     <!-- iCheck -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/jqvmap/jqvmap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/plugins/jqvmap/jqvmap.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('assets/dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/dist/css/adminlte.min.css')}}">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/daterangepicker/daterangepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/plugins/daterangepicker/daterangepicker.css')}}">
     <!-- summernote -->
-    <link rel="stylesheet" href="{{asset('assets/plugins/summernote/summernote-bs4.css')}}">
+    <link rel="stylesheet" href="{{asset('storage/assets/plugins/summernote/summernote-bs4.css')}}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <!-- Bootstrab 4.6-->
@@ -57,9 +57,9 @@
     </script>
    
     <!-- AdminLTE App -->
-    <script src="{{asset('assets/dist/js/adminlte.min.js')}}"></script>
+    <script src="{{asset('storage/assets/dist/js/adminlte.min.js')}}"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="{{asset('assets/dist/js/demo.js')}}"></script>
+    <script src="{{asset('storage/assets/dist/js/demo.js')}}"></script>
 
     <script>
         $(document).ready(function () {
@@ -72,25 +72,25 @@
     </script>
 
     <!-- JS Bootstrap 4.6 -->
-    <script src="{{asset('assets/js/bootstrap.bundle.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.min.js.map')}}"></script>
+    <script src="{{asset('storage/assets/js/bootstrap.bundle.js')}}"></script>
+    <script src="{{asset('storage/assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('storage/assets/js/bootstrap.js')}}"></script>
+    <script src="{{asset('storage/assets/js/bootstrap.min.js.map')}}"></script>
 
     <!-- OPTIONAL SCRIPTS -->
-    <script src="{{asset('assets/plugins/chart.js/Chart.min.js')}}"></script>
-    <script src="{{asset('assets/dist/js/demo.js')}}"></script>
-    <script src="{{asset('assets/dist/js/pages/dashboard3.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('storage/assets/dist/js/demo.js')}}"></script>
+    <script src="{{asset('storage/assets/dist/js/pages/dashboard3.js')}}"></script>
 
     <!-- daterangepicker -->
-    <script src="{{asset('assets/plugins/moment/moment.min.js')}}"></script>
-    <script src="{{asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/moment/moment.min.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
     <!-- Summernote -->
-    <script src="{{asset('assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
     <!-- overlayScrollbars -->
-    <script src="{{asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+    <script src="{{asset('storage/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 
     @stack('scrips')
 
